b23f4385c8597416ad3e1d5ac1c811f65291b015
Radu M <radu.moldoveanuf@gmail.com>
2023-08-02 19:28:45 +0300

    Added new arhitecture

--- src/main/java/com/spring_app/Main.java
+++ src/main/java/com/spring_app/Main.java
@@ -1,4 +1,6 @@
 package com.spring_app;
+import com.spring_app.model.Customer;
+import com.spring_app.repository.CustomerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -7,36 +9,8 @@ import java.util.List;
 
 
 @SpringBootApplication
-@RestController
-@RequestMapping ("api/customers")
 public class Main {
-    private final CustomerRepository customerRepository;
-
-    public Main(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-    public static void main(String[] args){
-        SpringApplication.run(Main.class,args);
-    }
-    @GetMapping
-    public List<Customer> getCusotmers(){
-        return customerRepository.findAll();
-    }
-    record NewCustomerRequest(String name,String email,Integer age){
-
-    }
-    @PostMapping
-    public void addCustomer(@RequestBody NewCustomerRequest request){
-        Customer customer=new Customer();
-        customer.setName(request.name());
-        customer.setEmail(request.email());
-        customer.setAge(request.age());
-        customerRepository.save(customer);
-    }
-
-    @DeleteMapping("{customerId}")
-    public void deleteCustomer(@PathVariable("customerId") Integer id){
-        customerRepository.deleteById(id);
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
